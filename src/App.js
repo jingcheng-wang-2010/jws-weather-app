34ede2a0dd6a80686b73ecb06cd3b2e1479ae030
Jingcheng Wang <jingwang49@gmail.com>
2024-01-22 22:16:51 +1100

    weather app ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import './App.css';
+import './styles.css'
+import 'semantic-ui-css/semantic.min.css'
+import React, { useEffect, useState } from 'react'
+import Weather from './components/Weather'
+import { Dimmer, Loader } from 'semantic-ui-react'
 
-function App() {
+export default function App () {
+  const [lat, setLat] = useState([])
+  const [long, setLong] = useState([])
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+      })
+
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+          console.log(result)
+        })
+    }
+    fetchData()
+
+    console.log('Latitude is:', lat)
+    console.log('Longitude is:', long)
+  }, [lat, long])
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      {typeof data.main != 'undefined' ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div>
+          {/*<Dimmer active>
+            <Loader>Loading..</Loader>
+      </Dimmer>*/}
+        </div>
+      )}
     </div>
-  );
+  )
 }
-
-export default App;
